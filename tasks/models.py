afc7e7897d2e7d016ccafd003babb6f7fe2bea7e
kivkiv12345 <kevincarlsen@live.dk>
2022-10-13 14:51:56 +0200

    2.4 Chain tasks & teams

--- tasks/models.py
+++ tasks/models.py
@@ -1,4 +1,4 @@
-from django.db.models import Model, IntegerField, CharField, ForeignKey, BooleanField, CASCADE, ManyToManyField
+from django.db.models import Model, IntegerField, CharField, ForeignKey, BooleanField, CASCADE, ManyToManyField, SET_NULL
 
 
 class Task(Model):
@@ -19,6 +19,7 @@ class Todo(Model):
 
 class Worker(Model):
     name = CharField(max_length=256)
+    todo = ForeignKey(Todo, on_delete=SET_NULL, null=True, related_name='workers')
 
     def __str__(self):
         return self.name
@@ -27,6 +28,7 @@ class Worker(Model):
 class Team(Model):
     name = CharField(max_length=256)
     workers = ManyToManyField(Worker, related_name='workers')
+    current_task = ForeignKey(Task, on_delete=SET_NULL, null=True, related_name='tasks')
 
     def __str__(self):
         return self.name
