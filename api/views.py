0acfcdebc2cd2d5edd5f95345ebea7a2bcf54afe
kevin <kevincarlsen@live.dk>
2022-11-03 09:35:56 +0100

    Basic support for generic web API crud operations

--- api/views.py
+++ api/views.py
@@ -1,8 +1,11 @@
+""" Generic creation of CRUD based web API views """
+
 from typing import Type
 
 from django.db.models import Model
 from django.shortcuts import render
 from django.urls import path
+from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework.serializers import ModelSerializer
@@ -14,11 +17,21 @@ from rest_framework.serializers import ModelSerializer
 #     person = {'name': 'Dennus', 'age': 28}
 #     return Response()
 
-_GET_SUFFIX = '_get'
-_POST_SUFFIX = '_post'
+_LIST_SUFFIX = '-list'
+_DETAIL_SUFFIX = '-detail'
+_CREATE_SUFFIX = '-create'
+_UPDATE_SUFFIX = '-update'
+_DELETE_SUFFIX = '-delete'
 
 
-def generic_crud(crud_model: Type[Model]):
+def generic_crud(crud_model: Type[Model]) -> tuple[path, path, path, path, path]:
+    """
+    Creates generic CRUD views for the specified model
+
+    :param crud_model: Model o create web API CRUD operations for
+
+    :returns: A tuple of path() instances to be inserted into your app's urlpatterns
+    """
 
     # Serializer = type('Serializer', (ModelSerializer, ), {
     #     'Meta': type('Meta', (), {
@@ -33,44 +46,74 @@ def generic_crud(crud_model: Type[Model]):
             fields = '__all__'
 
     @api_view(['GET'])
-    def generic_get(request):
-        items = crud_model.objects.all()
-        serializer = Serializer(items, many=True)
+    def generic_get_list(request: Request):
+        instances = crud_model.objects.all()
+        serializer = Serializer(instances, many=True)
+        return Response(serializer.data)
+
+    @api_view(['GET'])
+    def generic_get_detail(request: Request, pk: int):
+        instance = crud_model.objects.get(pk=pk)
+        serializer = Serializer(instance, many=False)
         return Response(serializer.data)
 
     @api_view(['POST'])
-    def generic_post(request):
+    def generic_create(request: Request):
         serializer = Serializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
         return Response(serializer.data)
 
+    @api_view(['POST'])
+    def generic_update(request: Request, pk: int):
+        instance = crud_model.objects.get(pk)
+        serializer = Serializer(instance=instance, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+        return Response(serializer.data)
+
+    @api_view(['DELETE'])
+    def generic_delete(request: Request, pk: int):
+        crud_model.objects.filter(pk=pk).delete()
+        return Response(200)
+
     model_name = crud_model.__name__.lower()
 
-    get_url = f"{model_name}{_GET_SUFFIX}"
-    post_url = f"{model_name}{_POST_SUFFIX}"
+    list_url = f"{model_name}{_LIST_SUFFIX}"
+    detail_url = f"{model_name}{_DETAIL_SUFFIX}"
+    create_url = f"{model_name}{_CREATE_SUFFIX}"
+    update_url = f"{model_name}{_UPDATE_SUFFIX}"
+    delete_url = f"{model_name}{_DELETE_SUFFIX}"
 
     return (
-        path(get_url, generic_get, name=get_url),
-        path(post_url, generic_post, name=post_url),
+        path(f"{list_url}/", generic_get_list, name=list_url),
+        path(f"{detail_url}/<str:pk>/", generic_get_detail, name=detail_url),
+        path(f"{create_url}/", generic_create, name=create_url),
+        path(f"{update_url}/<str:pk>/", generic_update, name=update_url),
+        path(f"{delete_url}/<str:pk>/", generic_delete, name=delete_url),
     )
 
 
 def crud_overview(urls: list[path]):
 
     overview_dict = {
-        'GET': [url.pattern._route for url in urls if url.name.endswith(_GET_SUFFIX)],
-        'POST': [url.pattern._route for url in urls if url.name.endswith(_POST_SUFFIX)],
+        'LIST': [url.pattern._route for url in urls if url.name.endswith(_LIST_SUFFIX)],
+        'DETAIL': [url.pattern._route for url in urls if url.name.endswith(_DETAIL_SUFFIX)],
+        'CREATE': [url.pattern._route for url in urls if url.name.endswith(_CREATE_SUFFIX)],
+        'UPDATE': [url.pattern._route for url in urls if url.name.endswith(_UPDATE_SUFFIX)],
+        'DELETE': [url.pattern._route for url in urls if url.name.endswith(_DELETE_SUFFIX)],
     }
 
     url_name = 'crud_overview'
 
     @api_view(['GET'])
     def overview_get(request):
+
+        # Show absolute URLs for the web API
         full_url = request._request.path
         url_prefix = full_url[:full_url.index(url_name)]
-        # TODO Kevin: Loop urls
-        return Response(overview_dict)
+        abs_url_dict = {key: [f"{url_prefix}{url}" for url in urls] for key, urls in overview_dict.items()}
+        return Response(abs_url_dict)
 
     # TODO Kevin: Not sure how I want to mutate the urlpatterns
     urls.append(path(url_name, overview_get, name=url_name))
