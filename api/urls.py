af13445773dad205afa860b402eb530795aad1fd
kevin <kevincarlsen@live.dk>
2022-12-14 19:59:11 +0100

    Add CRUD endpoints for Tasks app

--- api/urls.py
+++ api/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from .views import generic_crud, crud_overview
 from contoso_university.models import Student, Enrollment, Instructor, Course, Curriculum
+from tasks.models import Task, Todo, Worker, Team
 
 urlpatterns = [
     *generic_crud(Student),
@@ -9,6 +10,11 @@ urlpatterns = [
     *generic_crud(Course),
     *generic_crud(Curriculum),
     *generic_crud(Instructor),
+
+    *generic_crud(Task),
+    *generic_crud(Todo),
+    *generic_crud(Worker),
+    *generic_crud(Team),
 
 ]
 
